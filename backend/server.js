aa707866ee7158bc862d1575e7d17164ccfe981c
Toma <tomagvasile@gmail.com>
2025-03-10 16:26:26 +0200

    update server

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5000; 
 
 const corsOptions = {
   origin: 'https://anime-fox.netlify.app/',  
@@ -40,8 +40,8 @@ mongoose.connect('mongodb://localhost:27017/animeDB')
   .then(() => console.log('Successful connection to MongoDB'))
   .catch((error) => console.log('Connection error MongoDB:', error));
 
-app.listen(port, () => {
-  console.log(`Serverul rulează pe http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Serverul rulează pe ${PORT}`);
 });
 
 
