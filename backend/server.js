f2c6a96403390a4055a1008b5228cc0a5e9b31ad
Toma <tomagvasile@gmail.com>
2025-03-11 14:04:19 +0200

    up

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const path = require('path');
 const PORT = process.env.PORT || 5000; 
 
 const corsOptions = {
-  origin: 'https://anime-fox.netlify.app/',  
+  origin: '*',  
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
